df39c851e23f9172ee369f9c256a7d2cd9a14d31
Varun Gollapalli <gvarun1999@gmail.com>
2022-01-12 22:02:05 -0700

    Update SarthakAndHisMagicGrid.java

--- Exun 2021-2022/SarthakAndHisMagicGrid.java
+++ Exun 2021-2022/SarthakAndHisMagicGrid.java
@@ -1,3 +1,22 @@
+/*
+
+The key idea of this question is to understand how BITWISE XOR works.
+
+0 ^ 0 = 0
+0 ^ 1 = 1
+1 ^ 0 = 1
+1 ^ 1 = 0
+
+For all the bitwise XOR result to be same from any row, any column, or any of the two main diagonals - we can either fill 
+the entire 2D array with the same number. That way we will get the same result.
+
+Here, I filled the entire 2D array(arr[][]) with 1. 
+The bitwise XOR operation will yield 0. 
+
+
+*/
+
+
 import java.util.*;
 public class SarthakAndHisMagicGrid {
 
